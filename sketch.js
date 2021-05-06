c1676e9c28234283fb870527cf0b57bc5bebce88
Vinamra <jv.winny@gmail.com>
2021-05-06 14:51:05 +0530

    added log

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
-
+var log;
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -35,7 +35,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+    log = new Log(100,100,100,PI/2);
 }
 
 function draw(){
@@ -61,4 +61,5 @@ function draw(){
 
     bird.display();
     platform.display();
+    log.display();
 }
